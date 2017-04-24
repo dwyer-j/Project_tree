39209d2e5445ae9808a9db9976fcd2229e3a8f75
dwyer-j <dwyer_j@lynchburg.edu>
2017-04-24 16:29:22 -0400

    Add files via upload

---
+++ position.h
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstring>
+//#include "positionList.h"
+#include <vector>
+
+template <typename T>
+class Position
+{
+public:
+   Position();
+   Position(T value): data_(value){}
+
+   ~Position();
+
+   T& operator* ();
+   Position<T> parent() const;
+   bool isRoot() const;
+private:
+   Position<T>* parent_;
+   std::vector<Position*> children_;
+   T& data_;
+};
+
+template<typename T>
+Position<T>::Position()
+{
+   
+}
+
+template<typename T>
+Position<T>::~Position()
+{
+
+}
+
+template<typename T>
+T& Position<T>::operator*()
+{
+   return data_;
+}
+
+template<typename T>
+Position<T> Position<T>::parent() const
+{
+   return parent_;
+}
+
+template<typename T>
+bool Position<T>::isRoot() const
+{
+   if(parent_ == NULL)
+      return true;
+}
